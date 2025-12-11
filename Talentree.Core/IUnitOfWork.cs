f28705061c0a8097283efcda1ed77730f9e2b1a0
Mohamed Alaa <mohamedalaa2556@gmail.com>
2025-12-11 18:00:08 +0200

    Implement Unit of Work pattern - coordinates multiple repository operations as a single transaction , Ensures all changes are committed or rolled back together

--- Talentree.Core/IUnitOfWork.cs
+++ Talentree.Core/IUnitOfWork.cs
@@ -8,8 +8,21 @@ using Talentree.Core.Repository.Contract;
 
 namespace Talentree.Core
 {
-    public interface IUnitOfWork : IAsyncDisposable
+    public interface IUnitOfWork
     {
+        /// <summary>
+        /// Returns a repository instance for the specified entity type
+        /// Multiple calls for the same type return the same cached instance
+        /// </summary>
+        /// 
+        /// <returns>Repository instance for the entity</returns>
+        IGenericRepository<TEntity> Repository<TEntity>() where TEntity : BaseEntity;
 
+
+        /// <summary>
+        /// Saves all pending changes to the database as a single transaction
+        /// </summary>
+        /// <returns>Number of affected rows</returns>
+        Task<int> CompleteAsync();
     }
 }
--- Talentree.Repository/UnitOfWork.cs
+++ Talentree.Repository/UnitOfWork.cs
@@ -1,12 +1,53 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Talentree.Core;
+using Talentree.Core.Entities;
+using Talentree.Core.Repository.Contract;
+using Talentree.Repository.Data;
 
 namespace Talentree.Repository
 {
-    internal class UnitOfWork
+    internal class UnitOfWork : IUnitOfWork
     {
+        private readonly TalentreeDbContext _dbContext;
+
+        // Hashtable stores repository instances to avoid creating duplicates
+        // Key: Entity type name, Value: Repository instance
+        private readonly Hashtable _repositories;
+
+        public UnitOfWork(TalentreeDbContext dbContext)
+        {
+            _dbContext = dbContext;
+            _repositories = new Hashtable();
+        }
+        public IGenericRepository<TEntity> Repository<TEntity>() where TEntity : BaseEntity
+        {
+            // Use the entity type name as the key for caching
+            var entityType = typeof(TEntity).Name;
+            // Check if repository already exists in cache
+            if (!_repositories.ContainsKey(entityType))
+            {
+                // Create new repository instance and cache it
+                var repo = new GenericRepository<TEntity>(_dbContext);
+                _repositories.Add(entityType, repo);
+
+            }
+            // Return the cached repository (cast required due to Hashtable storing objects)
+            return (IGenericRepository<TEntity>)_repositories[entityType]!;
+        }
+
+        /// <summary>
+        /// Commits all pending changes to the database
+        /// All repository operations (Add, Update, Delete) are persisted here
+        /// </summary>
+        /// <returns>Number of entities affected by the operation</returns>
+        public async Task<int> CompleteAsync()
+            => await _dbContext.SaveChangesAsync();
+
+        // No need To dispose DbContext here as it is managed by the DI container
     }
 }
