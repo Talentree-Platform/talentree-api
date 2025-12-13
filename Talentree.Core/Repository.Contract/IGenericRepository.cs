919331f25b0024bf8d0dddec4034eba6b8f06e14
Mohamed Alaa <mohamedalaa2556@gmail.com>
2025-12-13 11:37:56 +0200

    Implement Generic Repo :- Responsibilities: - Acts as a centralized data access layer for all entities - Encapsulates Entity Framework Core logic - Works with Specification Pattern for filtering, sorting, includes, and pagination - Does NOT call SaveChanges (handled by Unit of Work)

--- Talentree.Core/Repository.Contract/IGenericRepository.cs
+++ Talentree.Core/Repository.Contract/IGenericRepository.cs
@@ -4,13 +4,21 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Talentree.Core.Entities;
+using Talentree.Core.Specifications;
 
 namespace Talentree.Core.Repository.Contract
 {
     public interface IGenericRepository<T> where T : BaseEntity
     {
-       Task<IReadOnlyList<T>> GetAllAsync();
+        Task<IEnumerable<T>> GetAllAsync();
+        Task<T?> GetByIdAsync(int id);
+        void Add(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+        Task<IEnumerable<T>> GetAllWithSpecificationsAsync(ISpecifications<T> specifications);
+        Task<T?> GetByIdWithSpecificationsAsync(ISpecifications<T> specifications);
 
-       Task<T?> GetByIdAsync(int id);
+        Task<int> GetCountWithSpecificationsAsync(ISpecifications<T> specifications);
+
     }
 }
