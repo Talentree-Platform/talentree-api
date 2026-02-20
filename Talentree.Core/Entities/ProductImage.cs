d9d2473bf02ec44d6801b6ca3f98e07a6bbb2053
Mohamed Alaa <mohamedalaa2556@gmail.com>
2026-02-21 00:35:41 +0200

    Adding Category , Product and productImages Intities

---
+++ Talentree.Core/Entities/ProductImage.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Talentree.Core.Entities
+{
+    public class ProductImage : BaseEntity
+    {
+        public string ImageUrl { get; set; } = string.Empty;
+        public bool IsMain { get; set; } = false;
+        public int DisplayOrder { get; set; } = 0;
+
+        // FK
+        public int ProductId { get; set; }
+        public Product Product { get; set; } = null!;
+    }
+}
