10b9bfa08697710ae3ba558825d762e976e46edc
Mohamed Alaa <mohamedalaa2556@gmail.com>
2026-03-29 20:58:48 +0200

    Create Service Interface

---
+++ Talentree.Service/DTOs/AccountSettings/LoginHistoryDto.cs
@@ -0,0 +1,11 @@
+﻿namespace Talentree.Service.DTOs.AccountSettings
+{
+    public class LoginHistoryDto
+    {
+        public string IpAddress { get; set; } = string.Empty;
+        public string? DeviceInfo { get; set; }
+        public string? Location { get; set; }
+        public DateTime LoginAt { get; set; }
+        public bool IsSuccessful { get; set; }
+    }
+}
---
+++ Talentree.Service/DTOs/AccountSettings/PreferencesDto.cs
@@ -0,0 +1,10 @@
+﻿namespace Talentree.Service.DTOs.AccountSettings
+{
+    public class PreferencesDto
+    {
+        public string Timezone { get; set; } = string.Empty;
+        public string DateFormat { get; set; } = string.Empty;
+        public string CurrencyDisplay { get; set; } = string.Empty;
+        public string DashboardLayout { get; set; } = string.Empty;
+    }
+}
