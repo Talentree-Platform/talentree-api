252c58a7d03ef42e1bb012d737b733da3c31d0f1
Mohamed Alaa <mohamedalaa2556@gmail.com>
2026-03-29 20:55:19 +0200

    Create DTOs

---
+++ Talentree.Service/DTOs/AccountSettings/ChangePasswordDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Talentree.Service.DTOs.AccountSettings
+{
+    public class ChangePasswordDto
+    {
+        public string CurrentPassword { get; set; } = string.Empty;
+        public string NewPassword { get; set; } = string.Empty;
+        public string ConfirmNewPassword { get; set; } = string.Empty;
+    }
+}
