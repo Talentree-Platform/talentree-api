1195693ada5be0640bda06d1f3ec279210e9e454
Mohamed Alaa <mohamedalaa2556@gmail.com>
2026-04-19 11:47:12 +0200

    Create Reviews Dtos

---
+++ Talentree.Service/DTOs/Reviews/RespondToReviewDto.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Talentree.Service.DTOs.Reviews
+{
+    public class RespondToReviewDto
+    {
+        [Required]
+        [MaxLength(500, ErrorMessage = "Response cannot exceed 500 characters")]
+        public string Response { get; set; } = string.Empty;
+    }
+}
---
+++ Talentree.Service/DTOs/Reviews/ReviewAnalyticsDto.cs
@@ -0,0 +1,28 @@
+﻿namespace Talentree.Service.DTOs.Reviews
+{
+    public class ReviewAnalyticsDto
+    {
+        public double AverageRating { get; set; }
+        public int TotalReviews { get; set; }
+        public int TotalResponded { get; set; }
+        public double ResponseRate { get; set; } // percentage
+        public RatingDistributionDto Distribution { get; set; } = new();
+        public List<SentimentTrendDto> SentimentTrend { get; set; } = new();
+    }
+
+    public class RatingDistributionDto
+    {
+        public int FiveStar { get; set; }
+        public int FourStar { get; set; }
+        public int ThreeStar { get; set; }
+        public int TwoStar { get; set; }
+        public int OneStar { get; set; }
+    }
+
+    public class SentimentTrendDto
+    {
+        public string Month { get; set; } = string.Empty; // e.g. "2026-04"
+        public double AverageSentiment { get; set; }
+        public int ReviewCount { get; set; }
+    }
+}
