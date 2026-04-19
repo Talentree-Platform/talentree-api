523fdbcd9bdf3bbd1ba4609a1f42c5605097abfa
Mohamed Alaa <mohamedalaa2556@gmail.com>
2026-04-19 11:43:36 +0200

    .

---
+++ Talentree.Core/Specifications/ReviewSpecifications/ReviewByIdSpecification.cs
@@ -0,0 +1,14 @@
+﻿using Talentree.Core.Entities;
+
+namespace Talentree.Core.Specifications.ReviewSpecifications
+{
+    public class ReviewByIdSpecification : BaseSpecifications<ProductReview>
+    {
+        public ReviewByIdSpecification(int reviewId)
+            : base(r => r.Id == reviewId)
+        {
+            AddInclude(r => r.Product);
+            AddInclude(r => r.Customer);
+        }
+    }
+}
